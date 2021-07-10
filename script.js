b0acf4616002f7d68f7b58dbc084d620ff612609
Rguardado1009 <rguardado1009@gmail.com>
2021-07-09 22:31:05 -0400

    Added content to index.html,style.css, script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+h2.textContent = "this is my website"; 
 document.querySelector('body').appendChild(h2); 
+
+document.getElementById("name").addEventListener("mouseover", mouseOver);
+document.getElementById("name").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("name").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("name").style.color = "black";
+}
